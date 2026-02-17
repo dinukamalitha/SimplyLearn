7b5e396b7201805f1d64f3236dfdb1bbe6aba526
PRIYASHANKA.P.G.U.I <110629606+priyashankapgui@users.noreply.github.com>
2026-02-17 18:39:52 +0530

    Add the  Comment

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,8 +1,8 @@
 const nodemailer = require("nodemailer");
-
+// Function to send email
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE, // e.g., 'gmail'
+    service: process.env.EMAIL_SERVICE,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
